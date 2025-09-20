a91ecacd49fd41ff53d713d07bc947d3a25e9f9d
YaninaA-V <JaninaVit92@yandex.ru>
2025-09-20 17:58:15 +0300

    Подключение к БД TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppDataSource } from './ormconfig';
 
 @Module({
   imports: [
-    UsersModule,
-    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),    
+    TypeOrmModule.forRoot(AppDataSource.options),    
     JwtModule.registerAsync({
       global: true,
       imports: [ConfigModule],
@@ -28,6 +33,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         };
       },
     }),
+    UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
