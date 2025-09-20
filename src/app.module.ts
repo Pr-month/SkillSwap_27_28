c0b8bfd3dae9c867f96eef6928dc56d358b60c7f
yurydzal <yurydzal@yandex.ru>
2025-09-20 16:25:18 +0400

    Подключение Jwt модуля

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    JwtModule.registerAsync({
+      global: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const secret = configService.get<string>('JWT_SECRET');
+        const expiresIn = configService.get<string>('JWT_EXPIRES_IN') ?? '3600';
+
+        if (!secret) {
+          throw new Error('JWT_SECRET is not defined');
+        }
+
+        return {
+          secret,
+          signOptions: { expiresIn },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
