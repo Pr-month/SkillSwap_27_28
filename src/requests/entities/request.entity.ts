1f2ef5cf88ef8a686076648feb4b930ff9f01802
Daniil Glushakov <anydnny@gmail.com>
2025-10-06 23:11:46 +0300

    feature(requests entity): Добавлен entity requests

--- src/requests/entities/request.entity.ts
+++ src/requests/entities/request.entity.ts
@@ -1,1 +1,35 @@
-export class Request {}
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+import { RequestStatus } from '../requests.enums';
+import { Skill } from '../../skills/entities/skill.entity';
+
+@Entity('requests')
+export class Request {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  createdAt: Date;
+
+  @ManyToOne(() => User)
+  sender: User;
+
+  @ManyToOne(() => User)
+  receiver: User;
+
+  @Column({
+    type: 'enum',
+    enum: RequestStatus,
+    default: RequestStatus.PENDING,
+  })
+  status: RequestStatus;
+
+  @ManyToOne(() => Skill)
+  offeredSkill: Skill;
+
+  @ManyToOne(() => Skill)
+  requestedSkill: Skill;
+
+  @Column({ type: 'boolean', default: false })
+  isRead: boolean;
+}
