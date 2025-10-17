db98558bbd17fa2883718698ed3f7bbca8426a3b
Roland <machodin@yandex.ru>
2025-10-17 23:06:09 +0300

    added ws guard to request module

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -6,10 +6,11 @@ import { Request } from './entities/request.entity';
 import { User } from '../users/entities/user.entity';
 import { Skill } from '../skills/entities/skill.entity';
 import { NotificationsGateway } from 'src/notifications/notifications.gateway';
+import { WsJwtGuard } from 'src/notifications/guards/ws-jwt.guard';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Request, User, Skill])],
   controllers: [RequestsController],
-  providers: [RequestsService, NotificationsGateway],
+  providers: [RequestsService, NotificationsGateway, WsJwtGuard],
 })
 export class RequestsModule {}
