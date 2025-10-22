363b95978729d5f041ef7bc79cf3b620eb87a6c9
Ruslan Skv <skvirya@mail.ru>
2025-10-22 12:29:43 +0300

    Запуск E2E тестов должен запускаться на отдельной бд

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,7 +4,7 @@ import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
-import { Skill } from 'src/skills/entities/skill.entity';
+import { Skill } from '../skills/entities/skill.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Skill]), ConfigModule],
